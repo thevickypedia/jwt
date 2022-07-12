6577ff9bb71c0e0e8b763c263a44414e33a56fcd
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-07-11 20:42:45 -0500

    Write a basic decoder python script

---
+++ main.py
@@ -0,0 +1,11 @@
+import jwt
+
+key = "secret"
+payload = {'username': 'Jarvis'}
+token = jwt.encode(payload, key)
+print("Encoded token: %s" % token)
+try:
+    decoded = jwt.decode(jwt=token, key="secret", options={"verify_signature": True}, algorithms="HS256")
+    print("Decoded token: %s" % decoded)
+except jwt.exceptions.DecodeError as error:
+    print(error)
