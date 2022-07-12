e02d3adff0d4a7576bed76c9b2400469d405a35e
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-07-11 21:22:05 -0500

    Add key-value pairs dynamically in HTML

---
+++ app.js
@@ -0,0 +1,95 @@
+function addRow() {
+
+    var key = document.getElementById("key");
+    var value = document.getElementById("value");
+
+    if (!key.value || !value.value) {
+        alert("Neither 'Key' nor 'Value' can be null");
+        return
+    }
+
+    var table = document.getElementById("myTableData");
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= key.value;
+    row.insertCell(2).innerHTML= value.value;
+
+}
+
+function deleteRow(obj) {
+
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+
+}
+
+function tableCells(t){
+   if(t.cells) return t.cells; // use internal routine when supported
+   for(var a=[], r=t.rows, y=0, c, x; t=r[y++];){
+      for(c=t.cells, x=0; t=c[x++]; a.push(t));
+   }
+   return a;
+}
+
+function addTable(e) {
+    e.preventDefault();
+    var secret = $("#secret").val();
+    var table = document.getElementById("myTableData");
+//    for (var i = 0, row; row = table.rows[i]; i++) {
+//        console.log(row.innerHTML);
+//       for (var j = 0, col; col = row.cells[j]; j++) {
+//            console.log(col.innerHTML);
+//       }
+//    }
+    var array = [];
+    for (var i = 3, cell; cell = tableCells(table)[i]; i++) {
+        if (i === 3 || i % 3 == 0 ) { continue; }
+        array.push(cell.innerHTML);
+    }
+    var dict = {}
+    for (var j = 0; j < array.length - 1; j++) {
+        if (j === 0 || j % 2 == 0 ) {
+            dict[array[j]] = array[j+1]
+        }
+    }
+    tokenize(secret, dict);
+}
+
+function load() {
+    console.log("Page load finished");
+}
+
+function base64url(source) {
+  // Encode in classical base64
+  encodedSource = CryptoJS.enc.Base64.stringify(source);
+
+  // Remove padding equal characters
+  encodedSource = encodedSource.replace(/=+$/, '');
+
+  // Replace characters according to base64url specifications
+  encodedSource = encodedSource.replace(/\+/g, '-');
+  encodedSource = encodedSource.replace(/\//g, '_');
+
+  return encodedSource;
+}
+
+function tokenize(secret, payload) {
+    var header = {
+      "alg": "HS256",
+      "typ": "JWT"
+    };
+    var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+    var encodedHeader = base64url(stringifiedHeader);
+    console.log(payload);
+    var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(payload));
+    var encodedData = base64url(stringifiedData);
+    var token = encodedHeader + "." + encodedData;
+    var signature = CryptoJS.HmacSHA256(token, secret);
+    signature = base64url(signature);
+    var signedToken = token + "." + signature;
+    document.getElementById("token").innerHTML = signedToken;
+}
